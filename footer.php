664d69ebb0037fa92f99249cdb24164741cfcbc0
show <showtime0205@gmail.com>
2020-11-15 18:43:15 +0900

    cssとjsの読み込み

---
+++ footer.php
@@ -0,0 +1,42 @@
+	<!-- footer-menu -->
+	<div id="footer-menu">
+		<div class="inner">
+			<div class="footer-logo"><a href="/">TF-30</a></div><!-- /footer-logo -->
+			<div class="footer-sub">サブタイトルが入りますサブタイトルが入ります</div><!-- /footer-sub -->
+
+			<nav class="footer-nav">
+				<ul class="footer-list">
+					<li class="menu-item"><a href="#">メニュー1</a></li>
+					<li class="menu-item"><a href="#">メニュー2</a></li>
+					<li class="menu-item"><a href="#">メニュー3</a></li>
+					<li class="menu-item"><a href="#">メニュー4</a></li>
+					<li class="menu-item"><a href="#">メニュー5</a></li>
+				</ul>
+			</nav>
+
+		</div><!-- /inner -->
+	</div><!-- /footer-menu -->
+
+
+
+	<!-- footer -->
+	<footer id="footer">
+		<div class="inner">
+			<div class="copy">&copy; daily-trial WordPress theme All rights reserved.</div><!-- /copy -->
+			<div class="by">Presented by <a href="https://tokyofreelance.jp/" rel="noopener" target="_blank">東京フリーランス</a>
+			</div><!-- /by -->
+
+		</div><!-- /inner -->
+	</footer><!-- /footer -->
+
+	<div class="floating">
+		<a href="#"><i class="fas fa-chevron-up"></i></a>
+	</div>
+
+	<!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="js/script.js"></script> -->
+	<?php wp_footer(); ?>
+
+</body>
+
+</html>
