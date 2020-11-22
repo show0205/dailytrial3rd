c1e69f509a1d63c98a9296717dc1d4bb6f5c8847
show <showtime0205@gmail.com>
2020-11-22 16:32:54 +0900

    投稿IDで指定してpickup表示

--- index.php
+++ index.php
@@ -1,45 +1,6 @@
 <?php get_header(); ?>
 
-	<!-- pickup -->
-	<div id="pickup">
-		<div class="inner">
-
-			<div class="pickup-items">
-
-				<a href="#" class="pickup-item">
-					<div class="pickup-item-img">
-						<img src="<?php echo get_template_directory_uri() ?>/img/pickup1.png" alt="">
-						<div class="pickup-item-tag">カテゴリ名</div><!-- /pickup-item-tag -->
-					</div><!-- /pickup-item-img -->
-					<div class="pickup-item-body">
-						<h2 class="pickup-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</h2><!-- /pickup-item-title -->
-					</div><!-- /pickup-item-body -->
-				</a><!-- /pickup-item -->
-
-				<a href="#" class="pickup-item">
-					<div class="pickup-item-img">
-						<img src="<?php echo get_template_directory_uri() ?>/img/pickup2.png" alt="">
-						<div class="pickup-item-tag">カテゴリ名</div><!-- /pickup-item-tag -->
-					</div><!-- /pickup-item-img -->
-					<div class="pickup-item-body">
-						<h2 class="pickup-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</h2><!-- /pickup-item-title -->
-					</div><!-- /pickup-item-body -->
-				</a><!-- /pickup-item -->
-
-				<a href="#" class="pickup-item">
-					<div class="pickup-item-img">
-						<img src="<?php echo get_template_directory_uri() ?>/img/pickup3.png" alt="">
-						<div class="pickup-item-tag">カテゴリ名</div><!-- /pickup-item-tag -->
-					</div><!-- /pickup-item-img -->
-					<div class="pickup-item-body">
-						<h2 class="pickup-item-title">記事のタイトルが入ります記事のタイトルが入ります記事のタイトルが入ります</h2><!-- /pickup-item-title -->
-					</div><!-- /pickup-item-body -->
-				</a><!-- /pickup-item -->
-
-			</div><!-- /pickup-items -->
-
-		</div><!-- /inner -->
-	</div><!-- /pickup -->
+<?php get_template_part( '../template-parts/pickup' )?>
 
 
 	<!-- content -->
